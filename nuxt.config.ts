89b80ac49e0bb93e17ecb6cbd0bfb8814bc0514a
Zwe Htet <zwehtetnaing@andfun.biz>
2026-02-03 21:25:39 +0630

    setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
   
   runtimeConfig: {
     public: {
-      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || 'https://2d3d.pnpmyanmar.com'
+      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || 'http://localhost:8001'
     }
   },
   
